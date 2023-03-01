396a37160f41f64d05e3ab85dbdcc579ff3827c9
Josh Balch <64498806+Joshrbalch@users.noreply.github.com>
2023-02-28 21:01:33 -0600

    split classes

---
+++ node.cpp
@@ -0,0 +1,12 @@
+template <class valuetype, class keytype>
+
+class Node {
+    private:
+    keytype key;
+    valuetype value;
+    Node* left, right;
+    bool color;
+    
+
+    public:
+};
--- redBlack.cpp
+++ redBlack.cpp
@@ -150,16 +150,3 @@ class RBTree {
 
         }
 };
-
-template <class valuetype, class keytype>
-
-class Node {
-    private:
-    keytype key;
-    valuetype value;
-    Node* left, right;
-    bool color;
-    
-
-    public:
-};
